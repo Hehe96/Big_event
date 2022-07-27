673d4c70235e9a555e97bdd9780a772169b9b96d
hezhuangzds <he_zhuang163@163.com>
2022-07-28 00:06:53 +0800

    完成主页功能开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,4 +3,20 @@
 $.ajaxPrefilter(function (options) {
   // 拼接完整的地址
   options.url = 'http://www.liulongbin.top:3007' + options.url
+  //同意为有权限的接口 设置heasers请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = { Authorization: localStorage.getItem('token') || '' }
+  }
+  // 全局配置complete
+  // 不论ajax请求成功还是失败都会调用complete 函数
+  options.complete = function (res) {
+    // 在complete回调中 可以使用res.responseJSON 拿到服务器响应回来的信息
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      console.log('111111111')
+      // 1.强制清空token
+      localStorage.removeItem('token')
+      // 2.强制跳转到登录页
+      location.href = '/login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function () {
+  // 调用取用户信息函数
+  getUserinfo()
+})
+var layer = layui.layer
+// 获取用户信息
+function getUserinfo() {
+  $.ajax({
+    type: 'get',
+    url: '/my/userinfo',
+    success: function (res) {
+      if (res.status !== 0) {
+        return layer.msg('获取用户信息失败')
+      }
+
+      //调用renderAvater函数渲染用户头像
+      renderAvater(res.data)
+    }
+  })
+
+  // 渲染用户头像
+  function renderAvater(user) {
+    var name = user.username || user.nickname
+    $('#welcome').html('欢迎&nbsp&nbsp' + name)
+    if (user.user_pic !== null) {
+      $('.layui-nav-img').attr('src', layui - nav - img)
+    } else {
+      $('.layui-nav-img').hide()
+      $('.text-avatar').html(name[0].toUpperCase()).show()
+    }
+  }
+  // 点击退出
+  $('.layui-nav-item').click(function () {
+    layer.confirm('确定退出?', { icon: 3, title: '提示' }, function (index) {
+      // 1.清除本地token
+      localStorage.removeItem('token')
+      // 2.重新跳转登录页面
+      location.href = '/login.html'
+      // 关闭提示框
+      layer.close(index)
+    })
+  })
+}
